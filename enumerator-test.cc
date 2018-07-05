1a885d15ec55e1adc15be0b4759444dea32eeb9e
Jeff Cameron <jecameron@expedia.com>
2018-07-04 19:48:42 -0700

    Deindex() function for enumerator is working.

--- enumerator-test.cc
+++ enumerator-test.cc
@@ -78,6 +78,65 @@ TEST_CASE("Two pawns", "[Enumerator]") {
   REQUIRE(!e.Increment());
 }
 
+TEST_CASE("Two pawns sharing the same row", "[Enumerator]") {
+  Enumerator e(0, 0, 1, 1, 4, 3);
+  REQUIRE(e.NumPositions() == 12);
+  REQUIRE(e == Board("   -   -   -   - "
+                     " -   -   -   -   "
+                     "   -   -   -   - "
+                     " w   -   -   b   "
+                     "   -   -   -   - "
+                     " -   -   -   -   "
+                     "   -   -   -   - "
+                     " -   -   -   -   "));
+  e.Increment();
+  REQUIRE(e == Board("   -   -   -   - "
+                     " -   -   -   -   "
+                     "   -   -   -   - "
+                     " -   w   -   b   "
+                     "   -   -   -   - "
+                     " -   -   -   -   "
+                     "   -   -   -   - "
+                     " -   -   -   -   "));
+  e.Increment(2);
+  REQUIRE(e == Board("   -   -   -   - "
+                     " -   -   -   -   "
+                     "   -   -   -   - "
+                     " w   -   b   -   "
+                     "   -   -   -   - "
+                     " -   -   -   -   "
+                     "   -   -   -   - "
+                     " -   -   -   -   "));
+  e.Increment();
+  REQUIRE(e == Board("   -   -   -   - "
+                     " -   -   -   -   "
+                     "   -   -   -   - "
+                     " -   w   b   -   "
+                     "   -   -   -   - "
+                     " -   -   -   -   "
+                     "   -   -   -   - "
+                     " -   -   -   -   "));
+  e.Increment(7);
+  REQUIRE(e == Board("   -   -   -   - "
+                     " -   -   -   -   "
+                     "   -   -   -   - "
+                     " b   -   -   w   "
+                     "   -   -   -   - "
+                     " -   -   -   -   "
+                     "   -   -   -   - "
+                     " -   -   -   -   "));
+  REQUIRE(e.Increment());
+  REQUIRE(e == Board("   -   -   -   - "
+                     " -   -   -   -   "
+                     "   -   -   -   - "
+                     " w   -   -   b   "
+                     "   -   -   -   - "
+                     " -   -   -   -   "
+                     "   -   -   -   - "
+                     " -   -   -   -   "));
+  REQUIRE(!e.Increment());
+}
+
 TEST_CASE("Two kings", "[Enumerator]") {
   Enumerator e(1, 1, 0, 0, 0, 0);
   REQUIRE(e.NumPositions() == 992);
@@ -172,4 +231,62 @@ TEST_CASE("Cycle of 28M+", "[Enumerator]") {
                      "   W   W   -   - "
                      " B   B   B   b   "));
   REQUIRE(!e.Increment());
+}
+
+TEST_CASE("Deindex zero with small enumerator", "[Enumerator]") {
+  Enumerator e(0, 0, 1, 1, 0, 0);
+  e.Deindex(0);
+  REQUIRE(e == Board("   w   -   -   - "
+                     " -   -   -   -   "
+                     "   -   -   -   - "
+                     " -   -   -   -   "
+                     "   -   -   -   - "
+                     " -   -   -   -   "
+                     "   -   -   -   - "
+                     " -   -   -   b   "));
+}
+
+TEST_CASE("Deindex zero with big enumerator", "[Enumerator]") {
+  Enumerator e(1, 2, 3, 2, 4, 3);
+  e.Deindex(0);
+  REQUIRE(e == Board("   -   -   -   - "
+                     " -   -   -   -   "
+                     "   w   -   -   - "
+                     " w   b   b   b   "
+                     "   -   -   -   - "
+                     " -   -   -   -   "
+                     "   -   -   -   - "
+                     " B   W   W   -   "));
+}
+
+TEST_CASE("Deindex compare to increment, small", "[Enumerator]") {
+  Enumerator a(0, 0, 1, 1, 2, 2);  // This one gets deindexed.
+  Enumerator b(0, 0, 1, 1, 2, 2);  // This one gets incremented.
+  for (uint64 i = 0; i < b.NumPositions(); ++i) {
+    a.Deindex(i);
+    REQUIRE(a == b);
+    b.Increment();
+  }
+}
+
+TEST_CASE("Deindex compare to increment, all 4 piece types", "[Enumerator]") {
+  Enumerator a(1, 1, 1, 1, 4, 3);  // This one gets deindexed.
+  Enumerator b(1, 1, 1, 1, 4, 3);  // This one gets incremented.
+  const uint64 step = 499;
+  for (uint64 i = 0; i < b.NumPositions(); i += step) {
+    a.Deindex(i);
+    REQUIRE(a == b);
+    b.Increment(step);
+  }
+}
+
+TEST_CASE("Deindex compare to increment, 28M+ loop", "[Enumerator]") {
+  Enumerator a(3, 2, 1, 2, 0, 0);  // This one gets deindexed.
+  Enumerator b(3, 2, 1, 2, 0, 0);  // This one gets incremented.
+  const uint64 step = 1000007;
+  for (uint64 i = 0; i < b.NumPositions(); i += step) {
+    a.Deindex(i);
+    REQUIRE(a == b);
+    b.Increment(step);
+  }
 }
